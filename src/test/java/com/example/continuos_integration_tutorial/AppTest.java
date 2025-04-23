866de8393752edda4733f6a26d79785e15ce20a4
pippodima <pippodima99@gmail.com>
2025-04-23 15:41:41 +0200

    main.yml changes

--- src/test/java/com/example/continuos_integration_tutorial/AppTest.java
+++ src/test/java/com/example/continuos_integration_tutorial/AppTest.java
@@ -1,6 +1,5 @@
 package com.example.continuos_integration_tutorial;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
